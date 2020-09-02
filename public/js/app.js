8ac6d5cc02be75ca08393c00baf020c1f4eddaec
harshgupta80700 <harshgupta.80700@gmail.com>
2020-09-03 01:29:36 +0530

    more paramaters added to the index page

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,10 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const message1 = document.querySelector('#message-1')
 const message2 =  document.querySelector('#message-2')
+const message3 = document.querySelector('#message-3')
+const message4 =  document.querySelector('#message-4')
+const message5 = document.querySelector('#message-5')
+const message6 =  document.querySelector('#message-6')
 
 
 
@@ -18,6 +22,10 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     message1.textContent = ''
     message2.textContent = ''
+    message3.textContent = ''
+    message4.textContent = ''
+    message5.textContent = ''
+    message6.textContent = ''
     const location = search.value
     message1.textContent = 'Loading...'
     fetch('/weather?address='+location).then((response) => {
@@ -30,7 +38,11 @@ weatherForm.addEventListener('submit', (e) => {
                 console.log(data.location)
                 console.log(data.tempinC)
                 message1.textContent = 'Location: ' + data.location
-                message2.textContent = 'Temp in C: ' + data.tempinC
+                message2.textContent = 'Country: ' + data.country
+                message3.textContent = 'Latitude: ' + data.latitude
+                message4.textContent = 'Longitude: ' + data.longitude
+                message5.textContent = 'Temp in C: ' + data.tempinC
+                message6.textContent = 'Temp in F: ' + data.tempinF
             }
         })
     })
